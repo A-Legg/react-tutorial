43a50603da4ba6a43790d94663f30b9685fd856d
Adam Legg <adam.legg@gmail.com>
2015-05-24 12:15:00 -0600

    fixed records spelling error

--- app/models/recorc.rb
+++
@@ -1,2 +0,0 @@
-class Recorc < ActiveRecord::Base
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524180232) do
+ActiveRecord::Schema.define(version: 20150524181345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recorcs", force: :cascade do |t|
+    t.date     "date"
+    t.float    "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.string   "title"
     t.date     "date"
     t.float    "amount"
     t.datetime "created_at", null: false
